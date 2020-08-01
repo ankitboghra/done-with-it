a19bd792737b788aaba1f75d56ed673688703254
ankitboghra <ankitboghra@gmail.com>
2020-08-01 17:43:36 +0530

    building form image upload component

---
+++ app/hooks/useLocation.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+const useLocation = () => {
+  const [location, setLocation] = useState();
+
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestPermissionsAsync();
+      if (!granted) return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getLastKnownPositionAsync();
+      setLocation({ latitude, longitude });
+      console.log({ latitude, longitude });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return location;
+};
+
+export default useLocation;
